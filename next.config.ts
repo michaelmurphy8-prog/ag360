9d81ec301443249a56e807b4847027f755a373e1
Mike Murphy <michaelmurphy8@yahoo.com>
2026-02-18 11:02:50 -0600

    Fix next.config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["ag360-navy.vercel.app", "localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
