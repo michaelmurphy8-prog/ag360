38ff34f3677b98589e81c960b3183befe71ce3ed
Mike Murphy <michaelmurphy8@yahoo.com>
2026-03-11 13:58:52 -0600

    fix: remove (connect360) root page to resolve Next.js 15 manifest build error

--- app/(connect360)/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-
-export default function Connect360Root() {
-  const router = useRouter()
-  useEffect(() => { router.replace('/splash') }, [router])
-  return null
-}
